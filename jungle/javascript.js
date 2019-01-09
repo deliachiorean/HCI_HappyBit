b20bcc6c74a138749bd82b881fc19a37b075904c
deliachiorean <chiorean.delia@gmail.com>
2019-01-09 22:37:46 +0200

    finished mmemory_game and learn mountain

--- jungle/javascript.js
+++ jungle/javascript.js
@@ -2,88 +2,91 @@ window.onload=function(){
 	document.getElementById("my_audio").play().catch(function() {
 			// do something
 			document.getElementById("my_audio").play()
-	});
+  });
 }
 
-var nrClick=0;
-var firs;
-var second;
-var match=0;
-var cards=[];
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#game').show() }, 10000);
+  })
 
+const cards = document.querySelectorAll('.memory-card');
 
-function initial(){
-		var arrayUsed=new Array(9);
-		var num=new Array(16);
-	for (var i = 0; i < 9; i++)
-  {
-    arrayUsed[i] = 0;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let counter=0;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
   }
-	for (var i = 0; i < 16; i++)
-  {
-    randomNumber = Math.floor(Math.random()*9);
-	console.log(randomNumber);
-    if ((arrayUsed[randomNumber]==0 || arrayUsed[randomNumber]==1) && randomNumber!=0) 
-    {
-      arrayUsed[randomNumber]=arrayUsed[randomNumber]+1;
-	  num[i]=randomNumber;
-    }
-    else
-    {
-      i--;
-    }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  counter++;
+  console.log(counter);
+  if (counter==6){
+    var audio = document.getElementById("bravo")
+    audio.play();
+    refresh();
   }
-	for(var i=0;i<16;i++){
-		cards[i]="sources/c"+num[i]+".png";
-		console.log(cards[i]);
-	}
-}
-initial();
-
-function alegere(card){
-	if(nrClick==0)
-	{
-		first=card;
-		document.images[card].src=cards[card];
-		nrClick=1;
-	}
-	else if(nrClick==1)
-	{
-		nrClick=2;
-		second=card;
-		document.images[card].src=cards[card];
-		timer=setInterval(control,500);
-	}
-	else{
-		alert("you can click on an image once!!1");
-	}
-}
-function control(){
-	clearInterval(timer);
-	nrClick=0;
-	if(cards[first]==cards[second]){
-		match++;
-		if(match==8)
-		var audio = document.getElementById("bravo")
-		audio.play();
-	}else{
-		document.images[first].src="forest.jpg";
-		document.images[second].src="forest.jpg";
-	}
+  resetBoard();
 }
 
+function refresh() {
 
-function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.play();
+  setTimeout(function () {
+      location.reload()
+  }, 5000);
 }
 
-function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.pause();
-    thissound.currentTime = 0;
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
 }
 
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 
 
